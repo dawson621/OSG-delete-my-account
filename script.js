b56afc96be89ff519d50d7b941d20cad224eb038
dawson621 <122133472+dawson621@users.noreply.github.com>
2023-07-07 21:13:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-<script>
-var heart = document.querySelector('.heart');
-var audio = document.getElementById('myAudio');
+body { 
+  text-align: center; 
 
-heart.addEventListener('mouseenter', function() {
-  audio.play();
-});
-</script>
+}
+
+.heart {
+	font-size: 150px;
+	color: #e00;
+	animation: beat .25s infinite alternate;
+	transform-origin: center;
+}
+
+@keyframes beat{
+	to { transform: scale(1.4); }
+}
