e7c291372b4feeca4f2b99e318ccb981a0534674
dawson621 <122133472+dawson621@users.noreply.github.com>
2023-07-07 21:09:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,6 @@
 var heart = document.querySelector('.heart');
 var audio = document.getElementById('myAudio');
 
-function toggleAnimation() {
-  heart.classList.toggle('clicked');
-}
-
-heart.addEventListener('mouseover', toggleAnimation);
-heart.addEventListener('click', toggleAnimation);
 heart.addEventListener('mouseenter', function() {
   audio.play();
 });
